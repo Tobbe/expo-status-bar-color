19cb332156133c2e76a08b2690d4f9a3c6078c53
Tobbe Lundberg <tobbe@tlundberg.com>
2024-12-18 12:26:51 +0100

    RNScreens error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
+import { Platform, StyleSheet } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        navigationBarColor: "transparent",
+        statusBarBackgroundColor:
+          Platform.OS === "android" ? "transparent" : undefined,
+        statusBarStyle: "dark",
+        statusBarTranslucent: true,
+        contentStyle: styles.contentStyle,
+      }}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  statusbarIndexScreen: {
+    // Match the color of the image at the top of the screen
+    color: "#e1d1b9",
+  },
+  contentStyle: {
+    backgroundColor: "#fff9ed",
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>
+        [RNScreens] Attempt to register InsetsObserverProxy on
+        com.android.internal.policy.DecorView
+      </Text>
+    </View>
+  );
+}
