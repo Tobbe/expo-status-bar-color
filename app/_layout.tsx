522802ca74122fbcf5ffd16b5dda642dc1cde4ff
Tobbe Lundberg <tobbe@tlundberg.com>
2024-12-18 12:46:38 +0100

    dev build as well as expo go

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,5 @@
 import { Stack } from "expo-router";
-import { Platform, StyleSheet } from "react-native";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        navigationBarColor: "transparent",
-        statusBarBackgroundColor:
-          Platform.OS === "android" ? "transparent" : undefined,
-        statusBarStyle: "dark",
-        statusBarTranslucent: true,
-        contentStyle: styles.contentStyle,
-      }}
-    />
-  );
+  return <Stack screenOptions={{ statusBarTranslucent: true }} />;
 }
-
-const styles = StyleSheet.create({
-  statusbarIndexScreen: {
-    // Match the color of the image at the top of the screen
-    color: "#e1d1b9",
-  },
-  contentStyle: {
-    backgroundColor: "#fff9ed",
-  },
-});
